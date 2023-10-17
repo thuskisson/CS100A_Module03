291f63e4ea270952e86782168085408750933609
thuskisson <thuskisson@pga.com>
2023-10-16 22:32:45 -0700

    added a line to search for a name in roster

--- Exercise_1.py
+++ Exercise_1.py
@@ -10,10 +10,17 @@ while player != 'done':
         #Add the new player to the team roster
         team.append(player)
 
+#Print the roster in reverse order
 print('Your team roster in reverse is: ')
 team.reverse()
 for p in team:
     print("\t" + p)
+
+#Search the roster for "Travis" and return a statement
+if 'Travis' in team:
+    print("I can't wait to play! ")
+else:
+    print("Put me in coach, I'm ready to play. ")
 
 
 
